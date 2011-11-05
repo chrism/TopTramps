20fc14df64a97405d0b89c90d61dceaa2a20c76e
Chris Masters <chrismasters@gmail.com>
2011-11-05 04:03:41 +0100

    First commit clean rails using postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toptramps::Application.config.secret_token = '8a9e4d54d5e798f559e74030ab29d6480ab74202186374e0dea82564e17c37ea81ac33e5d326856bdc95f54fbac3168b485d3a4788cdf48958de586efe700550'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Toptramps::Application.config.session_store :cookie_store, key: '_toptramps_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Toptramps::Application.config.session_store :active_record_store
